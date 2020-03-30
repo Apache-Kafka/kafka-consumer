a31a98ca4f1c001aa80d09e3fa1ff5f8a2d738b9
Balaji Varadharajan <vbalaji215@gmail.com>
2020-03-30 09:33:00 -0400

    Updated consumer to consume from multiple partitions

--- src/main/java/com/kafka/consumer/client/KafkaIncrementConsumer.java
+++ src/main/java/com/kafka/consumer/client/KafkaIncrementConsumer.java
@@ -1,6 +1,7 @@
 package com.kafka.consumer.client;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +13,25 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class KafkaIncrementConsumer {
 
-    @KafkaListener(topics = "t_increments")
-    public void consumeMessage(String message){
-      log.debug("Message from Topic:" + message);
+    /**
+     * This method is to read message without key
+     * @param message
+     */
+//    @KafkaListener(topics = "t_increments")
+//    public void consumeMessage(String message){
+//      log.debug("Message from Topic:" + message);
+//    }
+
+    @KafkaListener(topics = "t_increments_mult_partitions", concurrency = "2")
+    public void consumeMessageWithKey(ConsumerRecord<String, String> message) throws InterruptedException {
+        log.debug("Partition: {}, Key: {}, Message: {}", message.partition(), message.key(), message.value());
+        /**
+         * Consume messages every second
+         * If "concurrency" parameter is not specified the one message from the queue
+         * will be consumed every second.
+         * If "concurrency" parameter is set to a value say n( e.g. 2, 3, etc), then n messages
+         * can be consumed every second
+         */
+        Thread.sleep(1000);
     }
 }
